16ca4bfe35a1272cfeda2fc6acc13dd4d8276878
Tanya <tatka480@gmail.com>
2019-03-02 23:40:14 +0200

    Updates

---
+++ precache-manifest.301ed3caf7b1bff8e55bf3571e4a70ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dff5ab02d493b3fbf7649afa3792515",
+    "url": "/my-app/static/media/search.3dff5ab0.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "c463cdaebd2fb0ef7927",
+    "url": "/my-app/static/js/main.c463cdae.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/my-app/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "c463cdaebd2fb0ef7927",
+    "url": "/my-app/static/css/main.6639ee6a.chunk.css"
+  },
+  {
+    "revision": "7fe0a22f450b382c9807f271c8c04ba2",
+    "url": "/my-app/index.html"
+  }
+];
